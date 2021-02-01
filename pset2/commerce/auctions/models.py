b314b0335222a957151d1a05fab4b66bbe9736a2
LakeSuburbia <sandermangelschots@telenet.be>
2021-02-01 22:22:27 +0100

    Listings model works
    Listing works but is not implemented yet. The listings model should be extended with more parameters, but it's a good start!
    
    The index page shows the listings using a for loop. I would rather load some little pieces of 'listings' html inside this part of the indexpage.

--- pset2/commerce/auctions/models.py
+++ pset2/commerce/auctions/models.py
@@ -1,6 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import ForeignKey
+from django.core.validators import MinValueValidator
 
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    seller = models.ForeignKey(User, on_delete=CASCADE, related_name="sellers")
+    product = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    price = models.DecimalField(max_digits=10 ,decimal_places=2, default=0, validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return f"{self.seller} is selling {self.product} for a minimum price of {self.price} euro."
