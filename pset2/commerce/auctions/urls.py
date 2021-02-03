fffc41305e9325dab0419a59466aaed149735540
LakeSuburbia <sandermangelschots@telenet.be>
2021-02-03 10:12:34 +0100

    Images work

--- pset2/commerce/auctions/urls.py
+++ pset2/commerce/auctions/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -6,7 +8,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
-    path("images/image-not-found.jpg", views.defaultImage, name="defimg")
-    
-]
+    path("register", views.register, name="register")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
