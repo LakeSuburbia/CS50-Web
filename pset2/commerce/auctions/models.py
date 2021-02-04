0ce333a9b7d00be00296eb179235cc8658767081
LakeSuburbia <sandermangelschots@telenet.be>
2021-02-04 22:54:29 +0100

    Added category

--- pset2/commerce/auctions/models.py
+++ pset2/commerce/auctions/models.py
@@ -16,6 +16,7 @@ class Listing(models.Model):
     description = models.CharField(max_length=256)
     price = models.DecimalField(max_digits=10 ,decimal_places=2, default=0, validators=[MinValueValidator(0)])
     image = models.URLField(default="https://www.thermaxglobal.com/wp-content/uploads/2020/05/image-not-found.jpg")
+    category = models.CharField(max_length=20)
 
     def __str__(self):
         return f"{self.seller} is selling {self.product} for a minimum price of {self.price} euro."
