933ea4b87689ecf6863d733df167b9af355613a9
LakeSuburbia <sandermangelschots@telenet.be>
2021-04-14 20:18:35 +0200

    Models Post & Likes added

--- pset4/project4/network/models.py
+++ pset4/project4/network/models.py
@@ -9,3 +9,10 @@ class Follows(models.Model):
     follower = models.ForeignKey('User', on_delete=models.CASCADE, related_name="follower")
     followee = models.ForeignKey('User', on_delete=models.CASCADE, related_name="followee")
     
+class Post(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    body = models.TextField(max_length=140)
+
+class Likes(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
