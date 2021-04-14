706a75b210e57d81516e5721644f9fe425dc5846
LakeSuburbia <sandermangelschots@telenet.be>
2021-04-14 18:41:40 +0200

    Follow model added

--- pset4/project4/network/models.py
+++ pset4/project4/network/models.py
@@ -4,3 +4,8 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Follows(models.Model):
+    follower = models.ForeignKey('User', on_delete=models.CASCADE, related_name="follower")
+    followee = models.ForeignKey('User', on_delete=models.CASCADE, related_name="followee")
+    
